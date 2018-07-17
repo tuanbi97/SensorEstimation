01eb92fbad802615cd81c40c8e5d747e41e7c156
Tuan <tuanbi97@gmail.com>
2018-07-16 23:22:29 -0500

    Testqt

---
+++ SensorStream.py
@@ -0,0 +1,87 @@
+import socket
+import thread
+from time import *
+import sys
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+def init():
+    ID = []
+    aZ = []
+    line.set_data(ID, aZ)
+    return line,
+
+
+def animate(i):
+    global aZ
+    global ID
+    global tmp1
+    global check
+    data = conn.recv(4096)
+    chunk = data.split()
+    #print(len(chunk))
+    for j in range(0, len(chunk)):
+        tmp = chunk[j]
+        #print (tmp)
+        tmp = float(tmp)
+        check += 1
+        if check % 2 == 0:
+            aZ = np.append(aZ, tmp1)
+            ID = np.append(ID, int(tmp))
+            print (tmp1, ' ', tmp)
+            if int(tmp) > xrange:
+                aZ = aZ[1:len(aZ)]
+                ID = ID[1:len(ID)]
+                plt.axis([int(tmp) - xrange + 1, int(tmp), -yrange, yrange])
+        else:
+            tmp1 = tmp
+    line.set_data(ID, aZ)
+
+    #print(str(len(ID)))
+    return line,
+
+HOST = ''
+PORT = 5555
+BUFSIZE = 4096
+
+serv = socket.socket()
+try:
+    serv.bind((HOST, PORT))
+    serv.listen(socket.SOMAXCONN)
+    conn, addr = serv.accept()
+except KeyboardInterrupt:
+    print "KeyboardInterrupt"
+    serv.close()
+    exit(1)
+
+f = open('test.txt', 'w')
+c = 0
+aX = []
+aY = []
+aZ = []
+ID = []
+
+tmp1 = 0.0
+oldid = -1
+
+xrange = 400
+yrange = 4
+color = 'b'
+fig = plt.figure()
+ax = plt.axes(xlim=(0, xrange), ylim=(-yrange, yrange))
+line, = ax.plot(ID, aZ)
+
+check = 0
+
+anim = animation.FuncAnimation(fig, animate, init_func=init, interval=10, blit=True)
+plt.show()
+
+    # print (len(aZ), ' ', len(ID), ' ', ID[len(ID) - 1])
+
+    # while True:
+    #     if len(L) % 2 == 0:
+    #         break
+
+serv.close()
+conn.close()
