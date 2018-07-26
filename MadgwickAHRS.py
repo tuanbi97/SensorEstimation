f5b310ed5c0b8187c8fb737c7b178c2f53b833db
Tuan <tuanbi97@gmail.com>
2018-07-26 14:17:36 -0500

    Add MadgwickAHRS

--- MadgwickAHRS.py
+++ MadgwickAHRS.py
@@ -61,13 +61,17 @@ class MadgwickAHRS:
             _4q2 = 4.0 * self.q2
             _8q1 = 8.0 * self.q1
             _8q2 = 8.0 * self.q2
+            q0q0 = self.q0 * self.q0
+            q1q1 = self.q1 * self.q1
+            q2q2 = self.q2 * self.q2
+            q3q3 = self.q3 * self.q3
 
             # Gradient decent algorithm corrective step
 
-            s0 = (_4q0 * self.q2**2) + (_2q2 * ax1) + (_4q0 * self.q1**2) -( _2q1 * ay1)
-            s1 = _4q1 * self.q3**2 - _2q3 * ax1 + 4.0 * self.q0**2 * self.q1 -( _2q0 * ay1) - _4q1 + (_8q1 * self.q1**2) +( _8q1 * self.q2**2) +( _4q1 * az1)
-            s2 = (4.0 * self.q0**2* self.q2) + _2q0 * ax1 + _4q2 * self.q3**2 -( _2q3 * ay1) - _4q2 + (_8q2 * self.q1**2) + (_8q2 * self.q2**2) +( _4q2 * az1)
-            s3 = (4.0 * self.q1**2* self.q3)- _2q1 * ax1 + 4.0 * self.q2**2 * self.q3 - _2q2 * ay1
+            s0 = (_4q0 * q2q2) + (_2q2 * ax1) + (_4q0 * q1q1) -( _2q1 * ay1)
+            s1 = _4q1 * q3q3 - _2q3 * ax1 + 4.0 * q0q0 * self.q1 -( _2q0 * ay1) - _4q1 + (_8q1 * q1q1) +( _8q1 * q2q2) +( _4q1 * az1)
+            s2 = (4.0 * q0q0* self.q2) + _2q0 * ax1 + _4q2 * q3q3 -( _2q3 * ay1) - _4q2 + (_8q2 * q1q1) + (_8q2 * q2q2) +( _4q2 * az1)
+            s3 = (4.0 * q1q1* self.q3)- _2q1 * ax1 + 4.0 * q2q2 * self.q3 - _2q2 * ay1
             recipNorm = self.invSqrt(s0**2+s1**2+s2**2+s3**2) # normalise step magnitude
             s0a = s0*recipNorm
             s1a = s1*recipNorm
@@ -87,9 +91,7 @@ class MadgwickAHRS:
         self.q3 += qDot4 * (1.0 / sampleFreq)
 
         # Normalise quaternion
-        qp01=self.q0**2+self.q1**2
-        qp23=self.q2**2+self.q3**2
-        recipNorm = self.invSqrt(qp01+qp23)
+        recipNorm = self.invSqrt(self.q0**2+self.q1**2+self.q2**2+self.q3**2)
 
         self.q0 *= recipNorm
         self.q1 *= recipNorm
